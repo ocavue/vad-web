12944b57a765b88ba0fc2ca8c693c5a638ce05ee
ocavue <ocavue@users.noreply.github.com>
2024-12-19 19:31:59 +1100

    docs: update links for vite and webpack (#24)

--- packages/vad-web/src/recording.ts
+++ packages/vad-web/src/recording.ts
@@ -33,14 +33,14 @@ export interface RecordingOptions {
    *
    * It is used to process audio in a separate thread with very low latency.
    *
-   * If you are using Vite, you can use the following import:
+   * If you are using [Vite](https://vite.dev/), you can use the following import:
    *
    * ```ts
    * // audioWorkletURL is a string pointing to the audio worklet script
    * import audioWorkletURL from 'vad-web/vad-audio-worklet?url'
    * ```
    *
-   * If you are using other bundlers like WebPack, you need copy the
+   * If you are using other bundlers like [webpack](https://webpack.js.org/), you need copy the
    * [`vad-audio-worklet.js`](https://unpkg.com/vad-web/dist/vad-audio-worklet.js)
    * file to your public directory, then set the `audioWorkletURL` to the path of the file:
    *
